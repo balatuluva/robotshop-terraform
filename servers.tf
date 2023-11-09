a79909f4a7286b70e89e05c7d281614179246a82
balatuluva <bala.tulva@gmail.com>
2023-11-09 06:49:23 +0000

    Terraform instance creation

--- servers.tf
+++ servers.tf
@@ -1,17 +1,29 @@
+data "aws_ami" "centos" {
+  owners = ["973714476881"]
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+}
 resource "aws_instance" "frontend" {
-  ami           = "ami-03265a0778a880afb"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
     Name = "frontend"
   }
 }
-output "frontend" {
-  value = aws_instance.frontend.public_ip
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z086778943HEZIHKI7U9"
+  name = "frontend-dev.gehana26.online"
+  type = "A"
+  ttl = 30
+  records = [aws_instance.frontend.private_ip]
 }
+#output "frontend" {
+#  value = aws_instance.frontend.public_ip
+#}
 
 resource "aws_instance" "backend" {
-  ami           = "ami-03265a0778a880afb"
+  ami           = "data.aws_ami.centos.image_id"
   instance_type = "t3.micro"
 
   tags = {
