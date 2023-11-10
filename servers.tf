84bf1f8594c1e1bc54742e672153d34beb51fb40
balatuluva <bala.tulva@gmail.com>
2023-11-10 07:02:19 +0000

    servers

--- servers.tf
+++ servers.tf
@@ -3,12 +3,16 @@ data "aws_ami" "centos" {
   most_recent = true
   name_regex = "Centos-8-DevOps-Practice"
 }
+data "aws_security_group" "allow-all" {
+  name = "allow-all"
+}
 variable "instance_type" {
-  default = "t3.micro"
+  default = "t3.small"
 }
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
+  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
     Name = "frontend"
