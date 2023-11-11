064e312e82ea83354afd6ea2d6877a3737d7322d
balatuluva <bala.tulva@gmail.com>
2023-11-11 08:21:02 +0000

    count variable

--- servers.tf
+++ servers.tf
@@ -9,31 +9,27 @@ data "aws_security_group" "allow-all" {
 variable "instance_type" {
   default = "t3.small"
 }
+variable "components" {
+  default = [ "frontend", "mongodb", "catalogue" ]
+}
 resource "aws_instance" "frontend" {
+  count         = length(var.components)
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
-    Name = "frontend"
+    Name = "var.components[count.index]"
   }
 }
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z086778943HEZIHKI7U9"
-  name = "frontend-dev.gehana26.online"
-  type = "A"
-  ttl = 30
-  records = [aws_instance.frontend.private_ip]
-}
+#resource "aws_route53_record" "frontend" {
+#  zone_id = "Z086778943HEZIHKI7U9"
+#  name = "frontend-dev.gehana26.online"
+#  type = "A"
+#  ttl = 30
+#  records = [aws_instance.frontend.private_ip]
+#}
 #output "frontend" {
 #  value = aws_instance.frontend.public_ip
 #}
 
-resource "aws_instance" "backend" {
-  ami           = "data.aws_ami.centos.image_id"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "backend"
-  }
-}
