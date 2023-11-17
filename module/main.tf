05717d5fb75651d21205108d80d3ef8d3664cfa3
balatuluva <bala.tulva@gmail.com>
2023-11-17 07:27:32 +0000

    modules in Terraform

---
+++ module/main.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+
+  tags = {
+    Name = var.component_name
+  }
+}
+
+resource "null_resource" "provisioner" {
+  depends_on = [aws_instance.instance, aws_route53_record.records]
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      user = "centos"
+      password = "DevOps321"
+      host = aws_instance.instance.private_ip
+    }
+    inline = [
+      "rm -rf robotshop-shell",
+      "git clone https://github.com/balatuluva/robotshop-shell.git",
+      "cd robotshop-shell",
+      "sudo bash ${var.component_name}.sh ${var.password}"
+    ]
+  }
+}
+
+resource "aws_route53_record" "records" {
+  zone_id = "Z086778943HEZIHKI7U9"
+  name = "${var.component_name}-dev.gehana26.online"
+  type = "A"
+  ttl = 30
+  records = [aws_instance.instance.private_ip]
+}
--- servers.tf
+++ servers.tf
@@ -14,45 +14,53 @@
 #}
 #variable "components" {}
 
-resource "aws_instance" "instance" {
-  for_each      = var.components
-  ami           = data.aws_ami.centos.image_id
-  instance_type = each.value["instance_type"]
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-
-  tags = {
-    Name = each.value["name"]
-  }
-}
-
-resource "null_resource" "provisioner" {
-  depends_on = [aws_instance.instance, aws_route53_record.records]
-  for_each = var.components
-  provisioner "remote-exec" {
-    connection {
-      type = "ssh"
-      user = "centos"
-      password = "DevOps321"
-      host = aws_instance.instance[each.value["name"]].private_ip
-    }
-    inline = [
-      "rm -rf robotshop-shell",
-      "git clone https://github.com/balatuluva/robotshop-shell.git",
-      "cd robotshop-shell",
-      "sudo bash ${each.value["name"]}.sh ${lookup(each.value, "password", "null")}"
-    ]
-  }
-}
-
-resource "aws_route53_record" "records" {
-  for_each = var.components
-  zone_id = "Z086778943HEZIHKI7U9"
-  name = "${each.value["name"]}-dev.gehana26.online"
-  type = "A"
-  ttl = 30
-  records = [aws_instance.instance[each.value["name"]].private_ip]
-}
+#resource "aws_instance" "instance" {
+#  for_each      = var.components
+#  ami           = data.aws_ami.centos.image_id
+#  instance_type = each.value["instance_type"]
+#  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+#
+#  tags = {
+#    Name = each.value["name"]
+#  }
+#}
+#
+#resource "null_resource" "provisioner" {
+#  depends_on = [aws_instance.instance, aws_route53_record.records]
+#  for_each = var.components
+#  provisioner "remote-exec" {
+#    connection {
+#      type = "ssh"
+#      user = "centos"
+#      password = "DevOps321"
+#      host = aws_instance.instance[each.value["name"]].private_ip
+#    }
+#    inline = [
+#      "rm -rf robotshop-shell",
+#      "git clone https://github.com/balatuluva/robotshop-shell.git",
+#      "cd robotshop-shell",
+#      "sudo bash ${each.value["name"]}.sh ${lookup(each.value, "password", "null")}"
+#    ]
+#  }
+#}
+#
+#resource "aws_route53_record" "records" {
+#  for_each = var.components
+#  zone_id = "Z086778943HEZIHKI7U9"
+#  name = "${each.value["name"]}-dev.gehana26.online"
+#  type = "A"
+#  ttl = 30
+#  records = [aws_instance.instance[each.value["name"]].private_ip]
+#}
 #output "frontend" {
 #  value = aws_instance.frontend.public_ip
 #}
 
+module "servers" {
+  for_each = var.components
+  source = "./module"
+  component_name = each.value["name"]
+  env = var.env
+  instance_type = each.value["instance_type"]
+  password = lookup(each.value, "password", "null")
+}
