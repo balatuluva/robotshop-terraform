be8618e86ce40e9e9e22866ef3908685558717d9
balatuluva <bala.tulva@gmail.com>
2023-11-22 06:25:22 +0000

    ansible setup

--- servers.tf
+++ servers.tf
@@ -56,8 +56,28 @@
 #  value = aws_instance.frontend.public_ip
 #}
 
-module "servers" {
-  for_each = var.components
+#module "servers" {
+#  for_each = var.components
+#  source = "./module"
+#  component_name = each.value["name"]
+#  env = var.env
+#  instance_type = each.value["instance_type"]
+#  password = lookup(each.value, "password", "null")
+#}
+
+module "database-servers" {
+  for_each = var.database_servers
+  source = "./module"
+  component_name = each.value["name"]
+  env = var.env
+  instance_type = each.value["instance_type"]
+  password = lookup(each.value, "password", "null")
+  provisioner = true
+}
+
+module "app-servers" {
+  depends_on = [module.database-servers]
+  for_each = var.app_servers
   source = "./module"
   component_name = each.value["name"]
   env = var.env
