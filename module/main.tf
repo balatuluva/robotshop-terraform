232be16b8cdf93c08f9439b24a020059743bf653
balatuluva <bala.tulva@gmail.com>
2024-01-29 14:02:38 +0000

    decrypt kms parameter for payment service correction

--- module/main.tf
+++ module/main.tf
@@ -68,18 +68,16 @@ resource "aws_iam_role_policy" "ssm-ps-policy" {
         "Sid": "VisualEditor0",
         "Effect": "Allow",
         "Action": [
+          "kms:Decrypt",
           "ssm:GetParameterHistory",
           "ssm:GetParametersByPath",
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource": "arn:aws:ssm:us-east-1:313587121011:parameter/${var.env}.${var.component_name}.*"
-      },
-      {
-        "Sid": "VisualEditor1",
-        "Effect": "Allow",
-        "Action": "ssm:Decrypt",
-        "Resource": "*"
+        "Resource": [
+          "arn:aws:kms:us-east-1:313587121011:key/b4ef00c9-7aa3-4bba-ad5d-54544604bbaa",
+          "arn:aws:ssm:us-east-1:313587121011:parameter/${var.env}.${var.component_name}.*"
+        ]
       }
     ]
   })
