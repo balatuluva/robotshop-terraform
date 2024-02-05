01a7a1278f0b15e03ff636c60e3f4f2e43464bbd
balatuluva <bala.tulva@gmail.com>
2024-02-05 07:39:23 +0000

    terraform monitor tags for app and db

--- module/locals.tf
+++ module/locals.tf
@@ -10,4 +10,11 @@ locals {
     "sudo labauto ansible",
     "ansible-pull -i localhost, -U https://github.com/balatuluva/robotshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
   ]
+  db_tags = {
+    Name = "${var.component_name}-${var.env}"
+  }
+  app_tags = {
+    Name = "${var.component_name}-${var.env}"
+    Monitor = "true"
+  }
 }
--- module/main.tf
+++ module/main.tf
@@ -3,12 +3,13 @@ resource "aws_instance" "instance" {
   instance_type = var.instance_type
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
   iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+  tags = var.app_type == "app" ? local.app_tags : local.db_tags
 
-  tags = {
-    Name = local.name
+  #tags = {
+    #Name = local.name
     #Name = var.component_name
     #Name = var.env != "" ? "${var.component_name}-${var.env}" : var.component_name terraform condition
-  }
+  #}
 }
 
 resource "null_resource" "provisioner" {
