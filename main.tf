31e983a453af0802afbf08759a0f4e5df36bf868
balatuluva <bala.tulva@gmail.com>
2024-02-25 06:59:00 +0000

    rds module setup correction

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "docdb" {
 module "rds" {
   source = "git::https://github.com/balatuluva/tf-module-rds.git"
 
-  for_each = var.docdb
+  for_each = var.rds
   subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
   allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
   engine_version = each.value["engine_version"]
